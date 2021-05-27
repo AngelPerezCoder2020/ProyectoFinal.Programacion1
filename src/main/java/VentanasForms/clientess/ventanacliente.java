ffcfeb2f768b64f216ae7b0bff46cc4c94545cd3
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2021-05-26 21:48:25 -0600

    Cambios en los codigos de las ventanas

--- src/main/java/VentanasForms/clientess/ventanacliente.java
+++ src/main/java/VentanasForms/clientess/ventanacliente.java
@@ -93,44 +93,6 @@ public class ventanacliente extends javax.swing.JFrame {
         x.SetEdad(tablaclientes.getValueAt(fi,3).toString());
     }//GEN-LAST:event_tablaclientesMouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ventanacliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ventanacliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ventanacliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ventanacliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ventanacliente().setVisible(true);
-            }
-        });
-    }
-    public javax.swing.JTable getTabla(){
-        return tablaclientes;
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton agregarclienbtn;
     private javax.swing.JLabel jLabel1;
